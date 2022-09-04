d35aa3be8c1e3c88bf5d5ad0d37deb09eac25bb3
ivandjoh <ivandi.djoh@gmail.com>
2022-09-05 05:19:50 +0800

    create home & components

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { Sidebar } from './Sidebar';
+export { Chat } from './Chat';
+export { Chats } from './Chats';
+export { Input } from './Input';
+export { Message } from './Message';
+export { Navbar } from './Navbar';
+export { Search } from './Search';
