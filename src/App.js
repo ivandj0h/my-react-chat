7cccdc5c0da87c3d3176f4ac81f3a6048fe1269f
ivandjoh <ivandi.djoh@gmail.com>
2022-09-05 03:48:28 +0800

    code cleanup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        My React Chat
-      </header>
+    <div>
+      Hello world!
     </div>
   );
 }
